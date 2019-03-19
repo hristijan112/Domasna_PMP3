69e7b55dbfd87d93ea1829a806e868b9a9b5d74a
hristijan112 <hristijan0stojkovski@gmail.com>
2019-03-19 23:00:20 +0100

    Design completed

--- app/src/main/java/com/example/domasna_pmp3/MainActivity.java
+++ app/src/main/java/com/example/domasna_pmp3/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.domasna_pmp3;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Fragment1.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnToSearch = findViewById(R.id.btnToSearch);
+
+        btnToSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    public void onFragmentInteraction(Uri uri) {
     }
 }
